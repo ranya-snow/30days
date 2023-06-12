76c748d00ca57dd21a3f6f0534d26150a7c626c5
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-06-12 10:54:30 +0200

    Update streamlit_days.py

--- streamlit_days.py
+++ streamlit_days.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-import plotfly.express as px
+import plotly.express as px
 from datetime import datetime
 
 #define functions
