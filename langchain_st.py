f3d5a67b05b9c96f9b7665bb388f73396a82bb7f
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-09-26 13:49:35 +0200

    Update langchain_st.py

--- langchain_st.py
+++ langchain_st.py
@@ -19,7 +19,7 @@ with st.form('my_form'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
     generate_response(text)
-
+st.stop()
 benefits = {
     'Benefits': [
         "Scalability", 
