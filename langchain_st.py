e5effc9882a3dc7e1dab88e17c3b5f6cef817336
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-09-26 14:06:22 +0200

    Update langchain_st.py

--- langchain_st.py
+++ langchain_st.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 from langchain.llms import OpenAI
 
-st.title('🦜🔗 Quickstart App')
+st.title('Data Platform Description')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
@@ -12,13 +12,13 @@ def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+# with st.form('my_form'):
+#   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+#   submitted = st.form_submit_button('Submit')
+#   if not openai_api_key.startswith('sk-'):
+#     st.warning('Please enter your OpenAI API key!', icon='⚠')
+#   if submitted and openai_api_key.startswith('sk-'):
+#     generate_response(text)
 
 benefits = {
     'Benefits': [
